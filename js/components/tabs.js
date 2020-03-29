52d4c3f53419bb96b26e1fc927fdf7f831cc84d7
Brian <brian.fly0623@gmail.com>
2020-03-29 20:15:58 +0530

    Updated tabs

--- js/components/tabs.js
+++ js/components/tabs.js
@@ -18,12 +18,12 @@ export default class Tabs extends EmpyrealComponent {
     constructor(el, options) {
         super(el);
         this.settings = { ...DEFAULTS, ...options };
-        
+
         this.$el = c(this.el);
 
         this.$tabs = this.$el.find(".tab");
 
-        this.$tabContents = this.$tabs.map(function(i, val) {
+        this.$tabContents = this.$tabs.map(function (i, val) {
             let id = val.getAttribute("data-target") || val.getAttribute("href");
             return document.querySelector(`div${id}`);
         });
@@ -53,13 +53,9 @@ export default class Tabs extends EmpyrealComponent {
         else this._handleTabOpen(this.$tabs.first());
     }
 
-    _handleTabOpen(e) {
-        if (typeof this.settings.onTabOpen === "function") {
-            this.settings.onTabOpen.call(this, e, this.$el)
-        }
-        let $tabPressed = c(e);
-
-        let id = $tabPressed.attr("data-target") || $tabPressed.attr("href");
+    _handleTabOpen(elem) {
+        let $activeTab = c(elem);
+        let id = $activeTab.attr("data-target") || $activeTab.attr("href");
 
         let $previousActiveTabContent = this.$tabContents.filter(".active");
         let $activeTabContent = this.$tabContents.filter(id);
@@ -67,40 +63,46 @@ export default class Tabs extends EmpyrealComponent {
         // Tab active color
         this.$tabs.removeClass("active");
         this.$tabs.removeClass(this.settings.activeTabClass);
-        $tabPressed.addClass("active");
-        $tabPressed.addClass(this.settings.activeTabClass);
+        $activeTab.addClass("active");
+        $activeTab.addClass(this.settings.activeTabClass);
 
         // Hiding and showing tab content
         $previousActiveTabContent.removeClass("active");
         $activeTabContent.addClass("active");
 
         // Moving Tab Indicator
-        this._handleResizeIndicator($tabPressed);
+        this.resizeIndicator();
     }
 
-    _handleResizeIndicator($tabPressed) {
-        let tabSize = $tabPressed.size();
+    _handleTabClick(e) {
+        e.preventDefault();
+        if (typeof this.settings.onTabOpen === "function") {
+            this.settings.onTabOpen.call(this, e, this.$el)
+        }
+        this._handleTabOpen(e.target);
+    }
+
+    resizeIndicator() {
+        let $active = this.$tabs.filter(".active");
+        let tabSize = $active.size();
         this.$tabIndicator.css({
             top: tabSize.height - 2 + "px",
             left: tabSize.left - this.$el.size().left + "px",
-            width: $tabPressed.outerWidth(true) + "px"
-        })
+            width: $active.outerWidth(true) + "px"
+        });
     }
 
     _setupEventHandlers() {
-        this.$tabs.on("click touchstart", (e) => {
-            e.preventDefault();
-            this._handleTabOpen(e.target);
-        });
+        this.$tabs.on("click", this._handleTabClick.bind(this));
 
         window.addEventListener(
             "resize",
-            (this.listeners[0] = this._handleResizeIndicator.bind(this))
+            (this.listeners[0] = this.resizeIndicator.bind(this))
         );
     }
 
     _removeEventHandlers() {
-        this.$tabs.off("click touchstart");
+        this.$tabs.off("click");
         window.removeEventListener("resize", this.listeners[0]);
     }
 
